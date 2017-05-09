7ce7329db8b50af0ddd8c97116387fca49bca056
jimjunker1 <james.junker1@gmail.com>
2017-05-09 12:12:31 -0600

    uploading the bug data and subset by stream

--- SFS2017_script.R
+++ SFS2017_script.R
@@ -11,7 +11,9 @@ library(ggplot2)
 library(chron)
 
 #set ggplot theme
+source("C:/Users/Jim/Documents/Projects/General Files/General R scripts/theme_black.txt")
 theme_set(theme_bw(20))
+theme_set(theme_black(18))
 #import the data for 
 ##Time, temp, and light
 
@@ -37,19 +39,31 @@ temp7$Pd <- as.POSIXct(temp7$time, format = "%d-%m-%y %H:%M", tz = "UTC")
 light$Pd <- as.POSIXct(paste(light$Date, light$Time),format = "%m/%d/%y %H:%M:%S", tz = "UTC")
 light.est$Pd <- as.POSIXct(paste(light.est$date, light.est$time), format = "%m/%d/%y %H:%M:%S", tz = "UTC")
 
-#neeed to aggregate light estimates.
-lighthr_d <- aggregate(light["Intensity"], list(hour = cut(light$Pd, breaks = "hour")),mean, na.rm = T)
+#neeed to aggregate temp and light to daily
+temp9_d = aggregate(temp9["st9_tempC"], list(day = cut(temp9$Pd, breaks ="day ")), mean, na.rm = T)
+temp6_d = aggregate(temp6["st6_tempC"], list(day = cut(temp6$Pd, breaks = "day")), mean, na.rm = T)
+temp14_d = aggregate(temp14["st14_tempC"], list(day = cut(temp14$Pd, breaks = "day")), mean, na.rm = T)
+tempHV_d = aggregate(tempHV["Hver_tempC"], list(day = cut(tempHV$Pd, breaks = "day")), mean, na.rm = T)
+temp7_d = aggregate(temp7["L_tempC"], list(day = cut(temp7$Pd, breaks = "day")), mean, na.rm = T)
 
-light.esthr_d <- aggregate(light.est["light"], list(hour = cut(light.est$Pd, breaks = "hour")),mean, na.rm = T)
+lighthr_d <- aggregate(light["Intensity"], list(day = cut(light$Pd, breaks = "day")),mean, na.rm = T)
+light.esthr_d <- aggregate(light.est["light"], list(day = cut(light.est$Pd, breaks = "day")),mean, na.rm = T)
 
-lighthr_d$Pd <- as.POSIXct(lighthr_d$hour, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
-light.esthr_d$Pd <- as.POSIXct(light.esthr_d$hour, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+#reassign the Pd to a POSIX object
+temp9_d$Pd <- as.POSIXct(temp9_d$day, format = "%Y-%m-%d", tz = "UTC")
+temp6_d$Pd <- as.POSIXct(temp6_d$day, format = "%Y-%m-%d", tz = "UTC")
+temp14_d$Pd <- as.POSIXct(temp14_d$day, format = "%Y-%m-%d", tz = "UTC")
+temp7_d$Pd <- as.POSIXct(temp7_d$day, format = "%Y-%m-%d", tz = "UTC")
+tempHV_d$Pd <- as.POSIXct(tempHV_d$day, format = "%Y-%m-%d", tz = "UTC")
+
+lighthr_d$Pd <- as.POSIXct(lighthr_d$day, format = "%Y-%m-%d", tz = "UTC")
+light.esthr_d$Pd <- as.POSIXct(light.esthr_d$day, format = "%Y-%m-%d", tz = "UTC")
 
 #merge all the data frames to build one.
-temp_light <- merge(tempHV[,c(2:3)], temp9[,c(2:3)], by = "Pd", all = T)
-temp_light <- merge(temp_light, temp7[,c(2:3)], by = "Pd", all = T)
-temp_light <- merge(temp_light, temp6[,c(2:3)], by = "Pd", all = T)
-temp_light <- merge(temp_light, temp14[,c(2:3)], by = "Pd", all = T)
+temp_light <- merge(tempHV_d[,2:3], temp9_d[,2:3], by = "Pd", all = T)
+temp_light <- merge(temp_light, temp7_d[,2:3], by = "Pd", all = T)
+temp_light <- merge(temp_light, temp6_d[,2:3], by = "Pd", all = T)
+temp_light <- merge(temp_light, temp14_d[,2:3], by = "Pd", all = T)
 temp_light <- merge(temp_light, lighthr_d[,c(2:3)], by = "Pd", all = T)
 temp_light <- merge(temp_light, light.esthr_d[,c(2:3)], by = "Pd", all = T)
 
@@ -61,6 +75,9 @@ summary(temp_light11.12)
 head(temp_light11.12)
 tail(temp_light11.12)
 
+
+#creating a data file with just the montly temperature data
+temp_light.m = aggregate(temp_light11.12[c("Hver_tempC", "st9_tempC", "st6_tempC", "st14_tempC", "L_tempC", "Intensity", "light")], list(month=cut(temp_light11.12$Pd, breaks = "month")), mean, na.rm = T)
 
 #plotting the data to look at the holes.
  ggplot(temp_light11.12, aes(x = Pd, y = Hver_tempC)) + geom_line() +
@@ -74,7 +91,71 @@ tail(temp_light11.12)
  ggplot(temp_light11.12, aes(x = Pd, y = Intensity)) + geom_line() +
     xlab("Date") + scale_y_continuous(expand = c(0,0)) + theme(panel.grid = element_blank())
  ggplot(temp_light11.12, aes(x = Pd, y = light)) +geom_line()
+ 
+#final plots of temp and light
+theme_set(theme_bw(18))
+L.plot = ggplot(temp_light11.12, aes(x = Pd)) + geom_line(aes(y = Intensity/1000)) + xlab("Date") + ylab("Intensity (10^3 lx)") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank());L.plot
 
+T.plot = ggplot(temp_light11.12, aes(x = Pd)) + geom_line(aes(y = Hver_tempC), colour = "grey") +
+    geom_line(aes(y = st9_tempC), colour = "light blue") +
+   geom_line(aes(y = st6_tempC), colour = "red") +
+   geom_line(aes(y = st14_tempC), colour = "blue") +
+   geom_line(aes(y = L_tempC), colour = "yellow") +
+  scale_y_continuous(position = "right") + scale_x_chron(labels = NULL) +
+   xlab("") + ylab("Temperature (C)") + theme(plot.background = element_rect(fill = "transparent", colour = NA), panel.background = element_rect(fill = "transparent", colour = NA), 
+                                              axis.ticks.x = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank());T.plot
+
+#######
+
+library(gtable)
+library(grid)
+
+g1 = ggplot_gtable(ggplot_build(L.plot))
+g2 = ggplot_gtable(ggplot_build(T.plot)) 
+
+pp = c(subset(g1$layout, name == "panel", se = t:r))
+#g = gtable_add_grob(g1, list(g2$grobs[[which(g2$layout$name == "panel")]], g2$grobs[[which(g2$layout$name == "ylab-r")]]), pp$t, pp$l, pp$b, pp$l)
+g = gtable_add_grob(g1, g2$grobs[[which(g2$layout$name == "panel")]], pp$t, pp$l, pp$b, pp$l)
+
+#axis tweaks
+ia = which(g2$layout$name == "axis-r")
+ga = g2$grobs[[ia]]
+ax = ga$children[[2]]
+#ax$widths = rev(ax$widths)
+#ax$grobs = rev(ax$grobs)
+ax$grobs[[1]]$x = ax$grobs[[1]]$x - unit(0.5, "npc") + unit(0.15, "cm")
+g = gtable_add_cols(g, g2$widths[g2$layout[ia,]$l], length(g$widths)- 1)
+g = gtable_add_grob(g, ax, pp$t, length(g$widths) - 1, pp$b)
+g = gtable_add_cols(g, width = unit(1, "cm"), length(g$widths))
+#grid.draw(g)
+
+
+#axis label
+ib = which(g2$layout$name == "ylab-r")
+gb = g2$grobs[[ib]]
+bx = gb$children[[1]]
+bx$width
+g = gtable_add_grob(g, bx, pp$t, length(g$width) - 1, pp$b, z = Inf)
+grid.draw(g)
+
+##
+
+g1 = ggplotGrob(L.plot)
+#g1 = gtable_add_cols(g1, unit(0, "mm"))
+g2 = ggplotGrob(T.plot)
+g = rbind(g1, g2, size = "first")
+g$widths = unit.pmax(g1$widths, g2$widths) 
+g$layout[grep("guide", g$layout$name), c("t", "b")] <- c(1,nrow = 1)
+grid.newpage()
+grid.draw(rbind(g1, g2, size = "last"))
+#what is the correlation between light and temperature
+ 
+ cor(temp_light11.12$Intensity, temp_light11.12$Hver_tempC, use = "pairwise.complete.obs")
+ cor(temp_light11.12$Intensity, temp_light11.12$st9_tempC, use = "pairwise.complete.obs")
+ cor(temp_light11.12$Intensity, temp_light11.12$st6_tempC, use = "pairwise.complete.obs")
+ cor(temp_light11.12$Intensity, temp_light11.12$st14_tempC, use = "pairwise.complete.obs")
+ cor(temp_light11.12$Intensity, temp_light11.12$L_tempC, use = "pairwise.complete.obs")
+ 
 #Now bring in epilithon data
  
 chla <- read.csv(file = "C:/Users/Jim/Documents/Projects/Iceland/Epilithon/Chla.csv", T)
@@ -133,6 +214,33 @@ day = as.numeric(days(chron(dates = as.character(warming_sub$Pd))))
 
 JULIAN = julian(month, day, year, origin = c(month = 01, day = 01, year = 2010))
 
+## now reads in file for secondary production of files ##
+
+#load in the merge function ##
+
+source("C:/Users/Jim/Documents/Projects/Iceland/Bug Samples/Secondary Production/Secondary Production R code suite/frac_merge/frac_merge_function.txt")
+
+## Load in the full file of sample counts ##
+options(max.print = 10000000)
+
+bugs_fin <- read.table("C:/Users/Jim/Documents/Projects/Iceland/Bug Samples/Secondary Production/Final data files/SamplesFull_fin.txt", header = T, sep = "\t", quote = "", strip.white = T, check.names = F)
+bugs_fin = bugs_fin[,c(1:47)]
+
+frac_merge(bugs_fin, "hengill")
+
+hengill_merge <- read.table(file = './hengill_merged.txt', header= T, sep = "\t", quote = "", strip.white = T, check.names = F, row.names = 1)
+
+#str(hengill_merge)
+
+hver_bugs = hengill_merge[which(hengill_merge$SITE == "Hver"),]
+st9_bugs = hengill_merge[which(hengill_merge$SITE == "ST9"),]
+st6_bugs = hengill_merge[which(hengill_merge$SITE == "ST6"),]
+st14_bugs = hengill_merge[which(hengill_merge$SITE == "ST14"),]
+
+unique(hver_bugs$TAXON)
+unique(st9_bugs$TAXON)
+unique(st6_bugs$TAXON)
+unique(st14_bugs$TAXON)
 
 #########################
 
